28c4fdba37c9c37e03288f5b7cb5009503c4808f
Egon Valdmees <egon.valdmees@gmail.com>
2014-08-11 22:48:36 +0300

    Linker class

--- server.py
+++ server.py
@@ -56,69 +56,54 @@ def fq_url(*path, **subst):
 def href(url, **kw):
     return dict(kw, href=url)
 
-def create_linker(func):
-    def linker(item):
-        item.setdefault("_links", {}).update(func(item))
-        return item
-    return linker
+class Linker:
+    _links = {
+        "index": lambda item: {
+            "self": href(bottle.request.url),
+            "index": href(fq_url(ep.index)),
+            "venues": href(
+                fq_url(ep.venues),
+                params=["zip", "key_category", "location", "radius"]
+            ),
+            "categories": href(fq_url(ep.categories)),
+            "zips": href(fq_url(ep.zips))
+        },
+        "zips": lambda item: {
+            "self": href(fq_url(ep.zip, id_zip=item['zip'])),
+            "zip_venues": href(fq_url(ep.zip_venues, id_zip=item['zip']))
+        },
+        "categories": lambda item: {
+            "self": href(fq_url(ep.category, id_category=item['id'])),
+            "category_venues": href(fq_url(ep.category_venues, id_category=item['id']))
+        },
+        "venues": lambda item: {
+            "self": href(fq_url(ep.venue, id_venue=item['id'])),
+            "category": href(fq_url(ep.category, id_category=item['key_category'])),
+            "zip": href(fq_url(ep.zip, id_zip=item['zip']))
+        },
+    }
 
-def index_links(data):
-    linker = create_linker(lambda item: dict(
-        self = href(bottle.request.url),
-        index = href(fq_url(ep.index)),
-        venues = href(
-            fq_url(ep.venues),
-            params=["zip", "key_category", "location", "radius"]
-        ),
-        categories = href(fq_url(ep.categories)),
-        zips = href(fq_url(ep.zips))
-    ))
-    return linker(data)
-    #data.setdefault("_links", {}).update(
-    #    self = href(bottle.request.url),
-    #    index = href(fq_url(ep.index)),
-    #    venues = href(
-    #        fq_url(ep.venues),
-    #        params=["zip", "key_category", "location", "radius"]
-    #    ),
-    #    categories = href(fq_url(ep.categories)),
-    #    zips = href(fq_url(ep.zips))
-    #)
-    #return data
+    def _create_linker(self, func):
+        def linker(item):
+            item.setdefault("_links", {}).update(func(item))
+            return item
+        return linker
 
-def venue_links(data, single=True):
-    linker = create_linker(lambda item: dict(
-        self = href(fq_url(ep.venue, id_venue=item['id'])),
-        category = href(fq_url(ep.category, id_category=item['key_category'])),
-        zip = href(fq_url(ep.zip, id_zip=item['zip']))
-    ))
-    if not single:
-        return index_links({"venues": map(linker, data)})
-    if isinstance(data, (list, tuple)):
-        data = data[0]
-    return index_links(linker(data))
+    def index(self, data):
+        linker = self._create_linker(self._links["index"])
+        return linker(data)
 
-def category_links(data, single=True):
-    linker = create_linker(lambda item: dict(
-        self = href(fq_url(ep.category, id_category=item['id'])),
-        category_venues = href(fq_url(ep.category_venues, id_category=item['id']))
-    ))
-    if not single:
-        return index_links({"categories": map(linker, data)})
-    if isinstance(data, (list, tuple)):
-        data = data[0]
-    return index_links(linker(data))
+    def __getattr__(self, name):
+        def func(data={}, single=True):
+            linker = self._create_linker(self._links[name])
+            if not single:
+                return self.index({name: map(linker, data)})
+            if isinstance(data, (list, tuple)):
+                data = data[0]
+            return self.index(linker(data))
+        return func
 
-def zip_links(data, single=True):
-    linker = create_linker(lambda item: dict(
-        self = href(fq_url(ep.zip, id_zip=item['zip'])),
-        zip_venues = href(fq_url(ep.zip_venues, id_zip=item['zip']))
-    ))
-    if not single:
-        return index_links({"zips": map(linker, data)})
-    if isinstance(data, (list, tuple)):
-        data = data[0]
-    return index_links(linker(data))
+linker = Linker()
 
 
 ### query builder
@@ -159,7 +144,7 @@ class Query:
 @app.route(ep.categories)
 @app.route(ep.category)
 @db
-def categories(db, id_category=None):
+def categories_handler(db, id_category=None):
     q = Query(db, """
         select id, name
         from venue.category
@@ -167,7 +152,7 @@ def categories(db, id_category=None):
     """)
     q.add("and id = %(id)s", id_category)
     return json.dumps(
-        category_links(q(id=id_category), id_category)
+        linker.categories(q(id=id_category), id_category)
     )
 
 
@@ -176,7 +161,7 @@ def categories(db, id_category=None):
 @app.route(ep.category_venues)
 @app.route(ep.zip_venues)
 @db
-def venues(db, id_venue=None, id_category=None, id_zip=None):
+def venues_handler(db, id_venue=None, id_category=None, id_zip=None):
     q = Query(db, """
         select v.id, v.name, v.zip, v.address, v.phone, v.key_category
              , ST_AsGeoJSON(v.loc) as location
@@ -220,16 +205,15 @@ def venues(db, id_venue=None, id_category=None, id_zip=None):
         )
 
     q.map['location'] = json.loads
-    print q.get_sql(), q.params
     return json.dumps(
-        venue_links(q(), id_venue)
+        linker.venues(q(), id_venue)
     )
 
 
 @app.route(ep.zips)
 @app.route(ep.zip)
 @db
-def zips(db, id_zip=None):
+def zips_handler(db, id_zip=None):
     if id_zip:
         zips = [{"zip": id_zip}]
     else:
@@ -237,14 +221,13 @@ def zips(db, id_zip=None):
             select distinct zip from venue.venue
         """)()
     return json.dumps(
-        zip_links(zips, id_zip)
+        linker.zips(zips, id_zip)
     )
 
 
-@app.route(ep.root)
 @app.route(ep.index)
 def index():
-    return index_links({})
+    return linker.index({})
 
 
 ### start
@@ -253,6 +236,5 @@ def index():
 app.run(
     host=config.address or '0.0.0.0',
     port=config.port or '8080',
-    reloader=config.reloader,
-    debug=True
+    reloader=config.reloader
 )
